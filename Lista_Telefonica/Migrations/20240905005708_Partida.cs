52eb3d6cff15466d6e4fb7b0c4068e2d51ef3a4a
DiogoVitor <diogovitorvg@gmail.com>
2024-09-04 22:35:52 -0300

    Lista_Telefonica_atualizada

---
+++ Lista_Telefonica/Migrations/20240905005708_Partida.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lista_Telefonica.Migrations
+{
+    /// <inheritdoc />
+    public partial class Partida : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ListaTelModels",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    nome = table.Column<string>(type: "TEXT", nullable: false),
+                    email = table.Column<string>(type: "TEXT", nullable: false),
+                    telefone = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ListaTelModels", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ListaTelModels");
+        }
+    }
+}
